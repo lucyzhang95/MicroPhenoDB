c636ec05d92579e12884f16f9e89d69f42cf836a
Lucy Zhang <bailin.zhang1116@gmail.com>
2025-04-07 15:35:13 -0700

    revise ncit2taxid fn with more cautions for unfound terms

--- MicroPhenoDB_parser.py
+++ MicroPhenoDB_parser.py
@@ -1,6 +1,8 @@
 import csv
-
+import os
 import requests
+import aiohttp
+import asyncio
 
 
 def read_txt_file(in_file):
@@ -33,24 +35,42 @@ def ncit2taxid(ncit_code: list) -> dict:
         url = "https://www.ebi.ac.uk/ols4/api/ontologies/ncit/terms"
         params = {"iri": iri}
 
-        response = requests.get(url, params=params)
-        if response.status_code == requests.codes.ok:
-            terms = response.json().get("_embedded").get("terms")[0]
-            taxid = terms.get("annotation").get("NCBI_Taxon_ID")[0]
-            name = terms.get("label")
-            for desc in terms.get("description"):
-                if taxid:
-                    ncit2taxid[name] = {"taxid": int(taxid), "ncit": code, "description": desc}
-                else:
-                    print(f"No NCBI Taxonomy ID found for NCIT:{ncit_code}")
-        else:
-            print(f"Failed to fetch NCIT:{ncit_code}")
+        try:
+            response = requests.get(url, params=params, timeout=3)
+            response.raise_for_status()
+        except requests.RequestException as e:
+            print(f"Failed to fetch NCIT code {code}: {e}")
+            continue
+
+        terms_list = response.json().get("_embedded", {}).get("terms", [])
+        if not terms_list:
+            print(f"Failed to find terms for NCIT: {code}")
+            continue
+
+        terms = terms_list[0]
+        annot = terms.get("annotation", {})
+        taxid_list = annot.get("NCBI_Taxon_ID", [])
+
+        if not taxid_list:
+            print(f"No NCBI Taxonomy ID found for NCIT: {code}")
+            continue
+        taxid = taxid_list[0]
+        name = terms.get("label", "Unknown")
+        description = next(iter(terms.get("description", [])), "")
+
+        ncit2taxid[name] = {
+            "taxid": int(taxid),
+            "ncit": code,
+            "description": description,
+        }
     return ncit2taxid
 
 
 if __name__ == "__main__":
-    NCTI = ["C111133", "C111204", "C112407"]
-    taxids = ncit2taxid(NCTI)
+    filename = os.path.join("downloads", "NCIT.txt")
+    NCIT = get_ncit_code(filename)
+    # NCIT = ["C85924", "C83526"]
+    taxids = ncit2taxid(NCIT)
     print(taxids)
     # mismatch for C111133 from the original database, so need to manually change the taxid to 357276
     # taxids["C111133"] = 357276
