a7afb96cb927a7a991049db7b8067a3694a4301b
Lucy Zhang <bailin.zhang1116@gmail.com>
2025-04-07 23:33:13 -0700

    add name list to get_ncit_code fn to get organisms do not have ncit; replace the name and description in fetch_ncit_taxid fn

--- MicroPhenoDB_parser.py
+++ MicroPhenoDB_parser.py
@@ -13,15 +13,18 @@ def read_txt_file(in_file):
             yield line
 
 
-def get_ncit_code(in_file) -> list:
+def get_ncit_code(in_file) -> [list, list]:
     obj = read_txt_file(in_file)
     ncit_codes = []
+    names = []
     # 80 entries do not have ncit code, 582 unique ncit codes (755 with redundancy)
     # 80 + 755 = 835 which matches the source
     for line in obj:
         if len(line) > 2 and "NCIT" in line[2]:
             ncit_codes.append(line[2].split("_")[1])
-    return ncit_codes
+        else:
+            names.append(line[0])
+    return ncit_codes, names
 
 
 async def fetch_ncit_taxid(session, ncit_code, notfound_ncit):
@@ -40,27 +43,35 @@ async def fetch_ncit_taxid(session, ncit_code, notfound_ncit):
                 print(f"Failed to find terms for NCIT:{ncit_code}")
             terms = terms_list[0]
             annot = terms.get("annotation")
+            name = terms.get("label")
+            description = next(iter(terms.get("description")), "")
             if "NCBI_Taxon_ID" in annot:
                 taxid_list = annot.get("NCBI_Taxon_ID")
                 taxid = taxid_list[0]
-                name = terms.get("label")
-                description = next(iter(terms.get("description")), "")
                 return name, {
                     "taxid": int(taxid),
                     "ncit": ncit_code,
                     "description": description,
                 }
             else:
-                notfound_ncit[terms.get("label")] = ncit_code
-                print(f"NO NCBI Taxonomy ID found for NCIT:{ncit_code}")
+                notfound_ncit[name] = {"ncit": ncit_code, "description": description}
+                # print(f"NO NCBI Taxonomy ID found for NCIT:{ncit_code}")
 
     except requests.exceptions.RequestException as e:
         print(f"Failed to connect ebi API: {e}")
 
 
 async def ncit2taxid(ncit_codes):
+    """
+
+    :param ncit_codes:
+    :return ncit2taxid: a dictionary mapping NCIT codes to taxids
+    {'Trichostrongylus colubriformis': {'taxid': 6319, 'ncit': 'C125969', 'description': 'A species of parasitic...'}}
+    :return notfound_ncit: a dictionary with NCIT codes failed to map taxid
+    {'Trypanosoma brucei gambiense': {'ncit': 'C125975', 'description': 'A species of parasitic flagellate protozoa...'}}
+    """
     ncit_codes = set(ncit_codes)
-    ncit2taxis = {}
+    ncit2taxids = {}
     notfound_ncit = {}
 
     async with aiohttp.ClientSession() as session:
@@ -70,17 +81,21 @@ async def ncit2taxid(ncit_codes):
     for result in results:
         if result:
             name, taxid_info = result
-            ncit2taxis[name] = taxid_info
-    return ncit2taxis, notfound_ncit.copy()
+            ncit2taxids[name] = taxid_info
+    return ncit2taxids, notfound_ncit
+
+
+def hard_code_ncit2taxid():
+    ncit2taxids, notfound_ncit = asyncio.run(ncit2taxid(NCIT))
 
 
 if __name__ == "__main__":
     filename = os.path.join("downloads", "NCIT.txt")
-    NCIT = get_ncit_code(filename)
+    NCIT, _ = get_ncit_code(filename)
     # NCIT = ["C85924", "C83526"]
     taxids, notfound = asyncio.run(ncit2taxid(NCIT))
-    # 15 organisms NCIT cannot map to taxid
     print(notfound)
-    print(len(notfound))
+    # print(len(notfound))
+    # 15 organisms NCIT cannot map to taxid
     # mismatch for C111133 from the original database, so need to manually change the taxid to 357276
     # Need to hard-coded for {"C111133": 357276, "C85924": 884684}
