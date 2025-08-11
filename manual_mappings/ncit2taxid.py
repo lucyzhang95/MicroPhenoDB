967c75e345efc21ddf870554b7366281e8605cba
Lucy Zhang <bailin.zhang1116@gmail.com>
2025-08-11 15:03:45 -0700

    move manually mapped entities into manual_mappings module

---
+++ manual_mappings/ncit2taxid.py
@@ -0,0 +1,58 @@
+# names in notfound_ncit
+MANUAL_TAXID_MAPPING_PATCHES = {
+    "human parainfluenza virus": {"taxid": 2905673},
+    "trichoderma": {"taxid": 5543},
+    "trichomonas vaginalis": {"taxid": 5722},
+    "trypanosoma brucei gambiense": {"taxid": 31285},
+    "malassezia furfur": {"taxid": 55194},
+    "clostridium cluster iv": {"taxid": 1689151},
+    "clostridium cluster xvi": {"taxid": 543347},
+    "powassan virus": {"taxid": 11083},
+    "mycobacterium xenopi": {"taxid": 1789},
+    "clostridiales xi": {"taxid": 186804},
+    "clostridiales xiii": {"taxid": 189325},
+    "alpha-amylase (aspergillus oryzae)": {
+        "taxid": 5062,
+        "description": "A fungus used in East Asia to saccharify rice, sweet potato, and barley in the making of alcoholic beverages such as sake and shochu, and also to ferment soybeans for making soy sauce and miso. It is one of the different koji molds used for food fermentation.[Wikipedia]",
+    },
+    "japanese encephalitis virus strain nakayama-nih antigen (formaldehyde inactivated)": {
+        "taxid": 11076,
+        "description": "A virus from the family Flaviviridae, part of the Japanese encephalitis serocomplex of nine genetically and antigenically related viruses, some of which are particularly severe in horses, and four of which, including West Nile virus, are known to infect humans. The enveloped virus is closely related to the West Nile virus and the St. Louis encephalitis virus. The positive sense single-stranded RNA genome is packaged in the capsid which is formed by the capsid protein.[Wikipedia]",
+    },
+}
+
+# existing taxids in ncits2taxids
+MANUAL_TAXID_MAPPING_OVERRIDES = {
+    "bacteroides dorei": {"taxid": 357276},  # NCIT mapped 357256 is wrong
+    "ruminococcaceae": {"taxid": 216572},  # NCIT mapped 541000 is obsolete
+    "peptococcus": {"taxid": 2740},  # NCIT mapped 2840 is wrong
+    "mumps virus": {"taxid": 2560602},  # NCIT mapped 11161 is obsolete
+    "lymphocytic choriomeningitis virus": {"taxid": 3052303},  # NCIT mapped 11623 is obsolete
+    "trichosporon": {"taxid": 5552},  # NCIT mapped 599816 is obsolete
+    "bacillus cereus": {"taxid": 1396},  # NCIT mapped 13968 is wrong
+    "bacillaceae": {"taxid": 186817},  # NCIT mapped 18681 is wrong
+    "actinobaculum": {
+        "taxid": 76833,
+        "description": "Actinobaculum is a bacterial genus in the family Actinomycetaceae.[Wikipedia]",
+    },
+    "rikenellaceae": {
+        "taxid": 171550,
+        "description": "Rikenellaceae is a family of Gram-negative bacteria described by Noel R. Krieg in 2015. It contains nine genera, five of which are validly published by the International Code of Nomenclature of Prokaryotes.[2] Bacteria with 16S ribosomal RNA highly similar to the Rikenella genus, as compared to the larger taxonomic order Bacteroidales, are classified in this family. This family consists of non-motile, rod-shaped bacteria that are tolerant of bile. Most Rikenellaceae species have been identified in the gastrointestinal tract microbiomes of various animals. Bacteria of this taxonomic family are elevated in the gut microbiomes of mice that are leptin-resistant obese and diabetic.[4] However, Rikenellaceae bacteria are depleted in the gut microbiomes of obese American adults, leading to reduced synthesis of butyrate and disrupted metabolism. Gut microbiomes with elevated levels of Rikenellaceae bacteria are associated with lupus and Alzheimer's disease in mice and colorectal cancer in humans.[Wikipedia]",
+    },
+    "bacillus coagulans": {
+        "taxid": 1398,
+        "description": "H. coagulans is a Gram-positive, catalase-positive, spore-forming, motile, facultative anaerobe rod that measures approximately 0.9 μm by 3.0 μm to 5.0 μm. It may appear Gram negative when entering the stationary phase of growth. The optimum temperature for growth is 50 °C (122 °F); the range of temperatures tolerated is 30–55 °C (86–131 °F). IMViC tests VP and MR (methyl red) are positive.[Wikipedia]",
+    },
+    "aspergillus clavatus": {
+        "taxid": 5057,
+        "description": "Aspergillus clavatus is a species of fungus in the genus Aspergillus with conidia dimensions 3–4.5 x 2.5–4.5 μm. It is found in soil and animal manure. The fungus was first described scientifically in 1834 by the French mycologist John Baptiste Henri Joseph Desmazières. The fungus can produce the toxin patulin, which may be associated with disease in humans and animals. This species is only occasionally pathogenic. Other sources have identified many species of Aspergillus as producing dry, hydrophobic spores that are easily inhaled by humans and animals. Due to the small size of the spores, about 70% of spores of A. fumigatus are able to penetrate into the trachea and primary bronchi and close to 1% into alveoli. Inhalation of spores of Aspergillus is a health risk. A. clavatus is allergenic, causing the occupational hypersensitivity pneumonitis known as malt-worker's lung.[Wikipedia]",
+    },
+    "alternaria alternata": {
+        "taxid": 5599,
+        "description": "Alternaria alternata is a fungus causing leaf spots, rots, and blights on many plant parts, and other diseases. It is an opportunistic pathogen on over 380 host species of plant. It can also cause upper respiratory tract infections and asthma in humans with compromised immunity.[Wikipedia]",
+    },
+    "sarcina": {
+        "taxid": 1266,
+        "description": "Sarcina is a genus of gram-positive cocci bacteria in the family Clostridiaceae. A synthesizer of microbial cellulose, various members of the genus are human flora and may be found in the skin and large intestine. The genus takes its name from the Latin word 'sarcina,' meaning pack or bundle, after the cuboidal (2x2x2) cellular associations they form during division along three planes. The genus's type species is Sarcina ventriculi, a variety found on the surface of cereal seeds, in soil, mud, and in the stomachs of humans, rabbits, and guinea pigs.[Wikipedia]",
+    },
+}
